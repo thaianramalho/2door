f0d5cabbda119f38ae52e702e8b7e1efd66af86c
Thaian Ramalho <thaianramalho32@gmail.com>
2022-12-13 21:22:11 -0300

    good icons added

--- components/home.js
+++ components/home.js
@@ -1,70 +1,57 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Image } from 'react-native';
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {faHouse, faUser, faBars, faBell} from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Home() {
   return (
     <View
-    style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 676767
-    }}>
-    
-    <LinearGradient
-      // Background Linear Gradient
-      colors={['rgba(0,0,0,0.8)', 'black']}
-      start={{ x: 0 , y: 0.25 }}
-      style={{
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        height: '100%',
-      }}
-    />
+            style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 676767
+            }}>
+            
+            <LinearGradient
+                // Background Linear Gradient
+                colors={['rgba(0,0,0,0.8)', 'black']}
+                start={{ x: 0 , y: 0.25 }}
+                style={{
+                    position: 'absolute',
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    height: '100%',
+                }}
+            />
 
-<LinearGradient
-      // Menu Linear Gradient
-      colors={['rgba(141,141,141,0.9)', 'rgba(240,240,240,0.25)']}
-      start={{ x: 0.5 , y: 0.5 }}
-      style={{
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        height: '12%',
-        bottom: 0,
-        borderRadius: 40/2,
-        borderColor: 'white'
-      }}
-    />
+        <LinearGradient
+            // Menu Linear Gradient
+            colors={['rgba(141,141,141,0.9)', 'rgba(240,240,240,0.25)']}
+            start={{ x: 0.5 , y: 0.5 }}
+            style={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                height: '12%',
+                bottom: 0,
+                borderRadius: 40/2,
+                borderColor: 'white'
+            }}
+            />
 
-<View style={styles.menuBar}>
-    <Image 
-        source={require('../img/homeIcon.png')} 
-        style={{flex: 1}}
-        />
 
-    <Image 
-        source={require('../img/menuBars.png')} 
-        style={{flex: 1}}
-        />
+        <View style={styles.menuBar}>
+            <FontAwesomeIcon id='user' icon={faHouse} />
+            <FontAwesomeIcon id='menu' icon={faBars}  />
+            <FontAwesomeIcon id='bell' icon={faBell} />
+            <FontAwesomeIcon id='user' icon={faUser} />
+        </View>
 
-    <Image 
-        source={require('../img/bellRing.png')} 
-        style={{flex: 1}}
-        />
-
-    <Image 
-        source={require('../img/profileIcon.png')}
-        style={{flex: 1}} 
-        />
-</View>
-
-  </View>
+    </View>
   );
 }
 
@@ -82,9 +69,7 @@ const styles = StyleSheet.create({
   },
   menuBar:{
     justifyContent:'space-between',
-    // flex:1,
+    // flex: 1,
     flexDirection:'row',
-    maxWidth:'40%',
-    bottom: 1
   }
 });
