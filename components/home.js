1c538e746ecc15414222a025b5a31df751db6612
Thaian Ramalho <thaianramalho32@gmail.com>
2022-12-14 00:17:03 -0300

    bottom menu done

--- components/home.js
+++ components/home.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
@@ -8,47 +8,28 @@ import {faHouse, faUser, faBars, faBell} from '@fortawesome/free-solid-svg-icons
 export default function Home() {
   return (
     <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: 676767
-            }}>
+        style={styles.backgroundPrincipal}>
         
-            <LinearGradient
-                // Background Linear Gradient
-                colors={['rgba(0,0,0,0.8)', 'black']}
-                start={{ x: 0 , y: 0.25 }}
-                style={{
-                    position: 'absolute',
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    height: '100%',
-                }}
-            />
+        <LinearGradient
+            // Background Linear Gradient
+            colors={['rgba(0,0,0,0.8)', 'black']}
+            start={{ x: 0 , y: 0.25 }}
+            style={styles.backgroundLinearGradient}
+        />
 
         <LinearGradient
             // Menu Linear Gradient
             colors={['rgba(141,141,141,0.9)', 'rgba(240,240,240,0.25)']}
             start={{ x: 0.5 , y: 0.5 }}
-            style={{
-                position: 'absolute',
-                left: 0,
-                right: 0,
-                height: '12%',
-                bottom: 0,
-                borderRadius: 40/2,
-                borderColor: 'white'
-            }}
-            />
+            style={styles.menuLinearGradient}
+        />
 
 
         <SafeAreaView style={styles.menuBar}>
-            <FontAwesomeIcon icon={faHouse} style={styles.iconesMenu} />
-            <FontAwesomeIcon icon={faBars} style={styles.iconesMenu} />
-            <FontAwesomeIcon icon={faBell} style={styles.iconesMenu} />
-            <FontAwesomeIcon icon={faUser} style={styles.iconesMenu} />
+            <FontAwesomeIcon icon={faHouse} size={40} style={styles.iconesMenu} />
+            <FontAwesomeIcon icon={faBars} size={40} style={styles.iconesMenu} />
+            <FontAwesomeIcon icon={faBell} size={40} style={styles.iconesMenu} />
+            <FontAwesomeIcon icon={faUser} size={40} style={styles.iconesMenu} />
         </SafeAreaView>
 
     </View>
@@ -61,14 +42,43 @@ background:{
     backgroundGradientTop: "#333333",
     backgroundGradientBottom: "#666666"
   },
+
   menuBar:{
-    justifyContent:'space-between',
     position: 'absolute',
     flexDirection:'row',
-    bottom: 50
+    bottom: 40,
   },
+
   iconesMenu:{
     height:50,
-    width:50
-  }
+    width:50,
+    marginRight: 30,
+    marginLeft: 30
+  },
+
+  menuLinearGradient:{
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    height: '12%',
+    bottom: 0,
+    borderRadius: 40/2,
+    borderColor: 'white'
+  },
+
+  backgroundLinearGradient:{
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
+  },
+
+  backgroundPrincipal: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 676767
+  },
+  
 });
